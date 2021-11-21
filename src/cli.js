1583e70aead5bbeb96781152927985e0692d7675
typ99 <superarkada@gmail.com>
2021-11-21 16:40:09 +0300

    eslint fix

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const helloUser = () => {
-  const userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
