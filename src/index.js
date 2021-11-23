b02fc61a868fefd7737f9b8730dacb33f63ccca4
typ99 <superarkada@gmail.com>
2021-11-24 01:51:26 +0300

    add common logic for all games

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+
+const startGame = (rule, game) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log(rule);
+
+  for (let i = 1; i <= 3; i += 1) {
+    const [question, correctAnswer] = game();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startGame;
