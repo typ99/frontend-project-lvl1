4da5762a144ab92aadec741faa4a1f5ae63a59f3
typ99 <superarkada@gmail.com>
2026-08-27 19:49:20 +0300

    logical error fix

--- games/prime.js
+++ games/prime.js
@@ -1,16 +1,12 @@
 const isPrime = (number) => {
-    if (number < 2 || number % 2 === 0) {
-        return false;
-    } else if (number === 2) {
-        return true;
-    }
+    if (number < 2) return false;
+    if (number === 2) return true;
+    if (number % 2 === 0) return false;
 
     const limit = Math.sqrt(number);
 
     for (let i = 3; i <= limit; i += 2) {
-        if (number % i === 0) {
-            return false;
-        }
+        if (number % i === 0) return false;
     }
 
     return true;
